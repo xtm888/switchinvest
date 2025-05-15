acbc69dcdc0a2b00121a469d596b655757a2a9cf
xtm888 <aioxtm.888@gmail.com>
2025-05-16 01:33:29 +0200

    refactor: update map markers to use local assets and upgrade React dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,6 @@ import { LanguageProvider } from "@/contexts/language-context"
 import { Playfair_Display } from 'next/font/google';
 import ClientLayoutWrapper from '@/components/client-layout-wrapper';
 import "./globals.css"
-import 'leaflet/dist/leaflet.css'; // Import Leaflet CSS globally
 
 // Configure Playfair Display font
 const playfair = Playfair_Display({
@@ -83,10 +82,6 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning className={`${playfair.variable} ${GeistSans.variable} ${GeistMono.variable}`}>
       <head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
         <meta name="theme-color" content="#ffffff" />
       </head>
       <body className="bg-white flex flex-col min-h-screen">
--- components/leaflet-map.tsx
+++ components/leaflet-map.tsx
@@ -1,6 +1,7 @@
 'use client'
 
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 
 // Fix leaflet's default icon path issue with webpack
@@ -13,8 +14,8 @@ L.Icon.Default.mergeOptions({
 
 // Custom gold marker icon for properties
 const goldIcon = new L.Icon({
-  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-gold.png',
-  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconUrl: '/images/marker-icon-gold.png',
+  shadowUrl: '/images/marker-shadow.png',
   iconSize: [25, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
@@ -23,8 +24,8 @@ const goldIcon = new L.Icon({
 
 // Create a custom teal icon for the HQ
 const tealIcon = new L.Icon({
-  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
-  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconUrl: '/images/marker-icon-2x-green.png',
+  shadowUrl: '/images/marker-shadow.png',
   iconSize: [25, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
